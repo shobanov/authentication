e5d88dd526a748d975f073e3c0521f1df62cd993
aleksej <shobanoff@gmail.com>
2022-05-08 17:27:32 +0300

    transfer to antd

--- client/src/pages/registration/Registration.tsx
+++ client/src/pages/registration/Registration.tsx
@@ -1,60 +1,138 @@
+import { Button, Form, Input } from 'antd';
 import { FC } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
 
-import { Button, Checkbox, Input, Title } from '../../components';
-import { RegistrationForm, LinkRouterDom, Nav, NameContainer } from './styles';
-
-type Inputs = {
-  firstName: string,
-  lastName: string,
-  email: string,
-  password: string,
-};
+// import { RegistrationForm, LinkRouterDom, Nav, NameContainer } from './styles';
 
 export const Registration: FC = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-  console.log({...register("firstName")});
+  const [form] = Form.useForm();
+  
+    const onFinish = (values: any) => {
+      console.log('Received values of form: ', values);
+    };
 
   return (
-    <RegistrationForm onSubmit={handleSubmit(onSubmit)}>
-      <Title title="Sign up" />
-      <NameContainer>
-        <Input
-          type="text"
-          placeholder="First Name *"
-          {...register("firstName")}
-        />
-        {errors.firstName && <span>This field is required</span>}
-        <Input
-          type="text"
-          placeholder="Last Name *"
-          {...register("lastName")}
-        />
-        {errors.lastName && <span>This field is required</span>}
-      </NameContainer>
-      <Input
-        type="email"
-        placeholder="Email Address *"
-        {...register("email")}
-      />
-      {errors.email && <span>This field is required</span>}
-      <Input
-        type="password"
-        placeholder="Password *"
-        {...register("password")}
-      />
-      {errors.password && <span>This field is required</span>}
-      <Checkbox
-        title="I want to receive inspiration, marketing promotions and updates via email."
-      />
-      <Button title="SIGN UP" />
-      <Nav>
-        <LinkRouterDom to="/auth">
-          Already have an account? Sign in
-        </LinkRouterDom>
-      </Nav>
-    </RegistrationForm>
+    <Form
+      style={{backgroundColor: 'gold'}}
+      
+      wrapperCol={{span: 8, offset: 8}}
+      form={form}
+      name="register"
+      size="large"
+      onFinish={onFinish}
+      scrollToFirstError
+    >
+      <Form.Item
+        
+        name="email"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input placeholder='E-mail' />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password placeholder='Password' />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('The two passwords that you entered do not match!'));
+            },
+          }),
+        ]}
+      >
+        <Input.Password placeholder='Confirm Password' />
+      </Form.Item>
+
+      <Form.Item
+        name="nickname"
+        tooltip="What do you want others to call you?"
+        rules={[
+          {
+            required: true, message: 'Please input your nickname!',
+            whitespace: true
+          }
+        ]}
+      >
+        <Input placeholder='Nickname' />
+      </Form.Item>
+    
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
+
+// return (
+//   <RegistrationForm onSubmit={handleSubmit(onSubmit)}>
+//     <Title title="Sign up" />
+//     <NameContainer>
+//       <Input
+//         type="text"
+//         placeholder="First Name *"
+//         {...register("firstName")}
+//       />
+//       {errors.firstName && <span>This field is required</span>}
+//       <Input
+//         type="text"
+//         placeholder="Last Name *"
+//         {...register("lastName")}
+//       />
+//       {errors.lastName && <span>This field is required</span>}
+//     </NameContainer>
+//     <Input
+//       type="email"
+//       placeholder="Email Address *"
+//       {...register("email")}
+//     />
+//     {errors.email && <span>This field is required</span>}
+//     <Input
+//       type="password"
+//       placeholder="Password *"
+//       {...register("password")}
+//     />
+//     {errors.password && <span>This field is required</span>}
+//     <Checkbox
+//       title="I want to receive inspiration, marketing promotions and updates via email."
+//     />
+//     <Button title="SIGN UP" />
+//     <Nav>
+//       <LinkRouterDom to="/auth">
+//         Already have an account? Sign in
+//       </LinkRouterDom>
+//     </Nav>
+//   </RegistrationForm>
+// );
