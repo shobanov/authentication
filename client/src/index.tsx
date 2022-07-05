1939c2117ce43631ef8f90e86544c2ee723db844
aleksej <shobanoff@gmail.com>
2022-07-05 21:25:26 +0300

    wrapp the app in Provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,16 +4,22 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
+import { Provider } from 'react-redux';
+import { setupStore } from './store/store';
 
+const store = setupStore();
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App /> 
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
 
 reportWebVitals();
