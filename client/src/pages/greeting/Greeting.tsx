99ee4fbfbbe35f86fe6ca1e71ccc3c37f1c01632
aleksej <shobanoff@gmail.com>
2022-08-08 09:59:56 +0300

    transfer to react query

--- client/src/pages/greeting/Greeting.tsx
+++ client/src/pages/greeting/Greeting.tsx
@@ -1,11 +1,36 @@
-import { Button } from '../../components';
+import { Button, Spinner } from '../../components';
+import { AuthApi } from '../../api/AuthApi';
 import { GreetingWrapper } from './styles';
 
+import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+
 export const Greeting = () => {
+	const navigate = useNavigate();
+
+	const { isLoading, mutateAsync, data } = useMutation(
+		'logout',
+		() => AuthApi.logout(),
+		{
+			onSuccess: () => {
+				localStorage.removeItem('token');
+				navigate('/login');
+			},
+		}
+	);
+
+	// 	const name = data.
+
 	return (
 		<GreetingWrapper>
+			{isLoading && <Spinner />}
+			<Button
+				title='Logout'
+				type='button'
+				handler={() => mutateAsync()}
+				disabled={isLoading}
+			/>
 			<h2>Congratulations, you have successfully logged in!</h2>
-			<Button title='Logout' type='button' />
 		</GreetingWrapper>
 	);
 };
