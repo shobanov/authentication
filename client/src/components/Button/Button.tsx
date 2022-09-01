5fed873abfe560af71321c547179d5017203d6d0
aleksej <shobanoff@gmail.com>
2022-09-01 16:16:52 +0300

    add eslint/prettier

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -3,15 +3,15 @@ import { forwardRef, PropsWithChildren } from 'react';
 import { ButtonStyled } from './styles';
 
 interface ButtonProps {
-	children: React.ReactNode;
-	type: 'button' | 'reset' | 'submit';
-	isLoading?: boolean;
-	disabled?: boolean;
-	onClick?: () => void;
+  children: React.ReactNode;
+  type: 'button' | 'reset' | 'submit';
+  isLoading?: boolean;
+  disabled?: boolean;
+  onClick?: () => void;
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-	(props: PropsWithChildren, ref) => {
-		return <ButtonStyled ref={ref} {...props} />;
-	}
+  (props: PropsWithChildren, ref) => {
+    return <ButtonStyled ref={ref} {...props} />;
+  }
 );
--- client/src/pages/password-update/PasswordUpdate.tsx
+++ client/src/pages/password-update/PasswordUpdate.tsx
@@ -1,56 +1,56 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
 import { ToastContainer } from 'react-toastify';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { Button, Input } from '../../components';
-import { usePassUpdateMutation } from './usePassUpdate';
-import { schema } from './validation';
 import { IPasswordUpdate } from '../../types';
+import { usePassUpdateMutation } from './usePassUpdate';
 import { PasswordUpdateForm, PasswordUpdateWrapper } from './styles';
+import { schema } from './validation';
 
 export const PasswordUpdate = () => {
-	const { mutate, isLoading, isSuccess } = usePassUpdateMutation();
-	const { link } = useParams();
+  const { mutate, isLoading, isSuccess } = usePassUpdateMutation();
+  const { link } = useParams();
 
-	const {
-		register,
-		handleSubmit: handleFormSubmit,
-		formState: { errors },
-	} = useForm<IPasswordUpdate>({
-		resolver: yupResolver(schema),
-		mode: 'onTouched',
-	});
+  const {
+    register,
+    handleSubmit: handleFormSubmit,
+    formState: { errors },
+  } = useForm<IPasswordUpdate>({
+    resolver: yupResolver(schema),
+    mode: 'onTouched',
+  });
 
-	const handleSubmit: SubmitHandler<IPasswordUpdate> = ({ password }) => {
-		mutate({ password, link });
-	};
+  const handleSubmit: SubmitHandler<IPasswordUpdate> = ({ password }) => {
+    mutate({ password, link });
+  };
 
-	return (
-		<PasswordUpdateWrapper>
-			<ToastContainer limit={1} />
-			<h2>Сreate a new password</h2>
-			<PasswordUpdateForm onSubmit={handleFormSubmit(handleSubmit)}>
-				<Input
-					type='password'
-					placeholder='New password'
-					validationError={errors.password?.message}
-					{...register('password')}
-				/>
-				<Input
-					type='password'
-					placeholder='Confirm password'
-					validationError={errors.passwordConfirm?.message}
-					{...register('passwordConfirm')}
-				/>
-				<Button
-					type='submit'
-					disabled={isLoading || isSuccess}
-					isLoading={isLoading}
-				>
-					change password
-				</Button>
-			</PasswordUpdateForm>
-		</PasswordUpdateWrapper>
-	);
+  return (
+    <PasswordUpdateWrapper>
+      <ToastContainer limit={1} />
+      <h2>Сreate a new password</h2>
+      <PasswordUpdateForm onSubmit={handleFormSubmit(handleSubmit)}>
+        <Input
+          type="password"
+          placeholder="New password"
+          validationError={errors.password?.message}
+          {...register('password')}
+        />
+        <Input
+          type="password"
+          placeholder="Confirm password"
+          validationError={errors.passwordConfirm?.message}
+          {...register('passwordConfirm')}
+        />
+        <Button
+          type="submit"
+          disabled={isLoading || isSuccess}
+          isLoading={isLoading}
+        >
+          change password
+        </Button>
+      </PasswordUpdateForm>
+    </PasswordUpdateWrapper>
+  );
 };
